e8b3b3408d5e5899b7aa3635e6b333a086fa214b
RatamaG <rafaeltamarag1@gmail.com>
2022-07-12 16:39:55 -0500

    ex5

---
+++ cmd/ex5/main.go
@@ -0,0 +1,42 @@
+package main
+
+import(
+	"net/http"
+	"encoding/json"
+	// "github.com/gorilla/ux"
+)
+
+type Player struct{
+
+FirstName string `json:"name"`
+LastName string `json:"last_name"`
+Birthday int `json:"birthday"`
+Genre string `json:"genre"`
+Sport string `json:"sport"`
+TeamName string `json:"team_name"`
+}
+
+type infoplayer []Player
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+
+		 playerinfo := infoplayer{
+			Player{"Ra","TamaG",30,"M","cricket","Caballeros de Kolkata"},
+			Player{"Fernando","Uribe",34,"M","Futbol","Club Deportivo Popular Junior Fútbol Club S.A."},
+			Player{"Imer","Machado",49,"M","Futbol","Naciopan"},
+			Player{"Legone","James",37,"M","Basketball","Lakers"},
+			Player{"Mariana","Pajon",30,"F","Bmx","Colombia"},
+			Player{"Russell","Wesbrick",33,"M","Basketball","Lakers"},
+			Player{"Caterine","Ibargüen",38,"F","Triple Salto","Colombia"},
+			Player{"Carlos","Bacca",35,"M","Futbol","Club Deportivo Popular Junior Fútbol Club S.A."},
+
+		}
+
+		json.NewEncoder(w).Encode(playerinfo)
+	})
+
+	http.ListenAndServe(":8000", nil)
+}
+
