6b2a40e23c9bdbb6c6a75cfa30085d9772fd2f00
RatamaG <rafaeltamarag1@gmail.com>
2022-07-13 09:51:47 -0500

    Adding id

--- Ex/ex5/cmd/ex5/main.go
+++ Ex/ex5/cmd/ex5/main.go
@@ -8,6 +8,7 @@ import (
 )
 
 type Player struct {
+	Id string `json:"uuid"`
 	FirstName string `json:"name"`
 	LastName  string `json:"last_name"`
 	Birthday  int    `json:"birthday"`
@@ -27,14 +28,14 @@ func main() {
 	http.HandleFunc("/players", func(w http.ResponseWriter, r *http.Request) {
 
 		playerinfo := infoplayer{
-			Player{"Ra", "TamaG", 30, "M", "cricket", "Caballeros de Kolkata"},
-			Player{"Fernando", "Uribe", 34, "M", "Futbol", "Club Deportivo Popular Junior Fútbol Club S.A."},
-			Player{"Imer", "Machado", 49, "M", "Futbol", "Naciopan"},
-			Player{"Legone", "James", 37, "M", "Basketball", "Lakers"},
-			Player{"Mariana", "Pajon", 30, "F", "Bmx", "Colombia"},
-			Player{"Russell", "Wesbrick", 33, "M", "Basketball", "Lakers"},
-			Player{"Caterine", "Ibargüen", 38, "F", "Triple Salto", "Colombia"},
-			Player{"Carlos", "Bacca", 35, "M", "Futbol", "Club Deportivo Popular Junior Fútbol Club S.A."},
+			Player{"47f1923a-0227-11ed-b939-0242ac120002","Ra", "TamaG", 30, "M", "cricket", "Caballeros de Kolkata"},
+			Player{"04ef0cad-c366-486b-8a98-8480b7a57442","Fernando", "Uribe", 34, "M", "Futbol", "Club Deportivo Popular Junior Fútbol Club S.A."},
+			Player{"47f1946a-0227-11ed-b939-0242ac120002","Imer", "Machado", 49, "M", "Futbol", "Naciopan"},
+			Player{"6ac7fa78-9b91-48c6-b8cf-45e917844d4b","Legone", "James", 37, "M", "Basketball", "Lakers"},
+			Player{"f37c12fd-e710-4d9b-99fa-fd4c60931b57","Mariana", "Pajon", 30, "F", "Bmx", "Colombia"},
+			Player{"47f196cc-0227-11ed-b939-0242ac120002","Russell", "Wesbrick", 33, "M", "Basketball", "Lakers"},
+			Player{"0ae0bdf1-2a75-405d-9470-9f525c9b597e","Caterine", "Ibargüen", 38, "F", "Triple Salto", "Colombia"},
+			Player{"47f196cc-0227-11ed-b939-0242ac120002","Carlos", "Bacca", 35, "M", "Futbol", "Club Deportivo Popular Junior Fútbol Club S.A."},
 		}
 
 		json.NewEncoder(w).Encode(playerinfo)
