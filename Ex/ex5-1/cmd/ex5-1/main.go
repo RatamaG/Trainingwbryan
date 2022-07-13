4a335dab93153228f827a9b93bd8009836175d71
RatamaG <rafaeltamarag1@gmail.com>
2022-07-13 17:57:36 -0500

    Organized

--- Ex/ex5-1/cmd/ex5-1/main.go
+++ Ex/ex5-1/cmd/ex5-1/main.go
@@ -3,23 +3,99 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
+	"time"
 )
 
 type Player struct {
-	Id string `json:"uuid"`
-	FirstName string `json:"name"`
-	LastName  string `json:"last_name"`
-	Birthday  int    `json:"birthday"`
-	Genre     string `json:"genre"`
-	Sport     string `json:"sport"`
-	TeamName  string `json:"team_name"`
+	ID        string    `json:"ID"`
+	FirstName string    `json:"name"`
+	LastName  string    `json:"last_name"`
+	Birthday  time.Time `json:"birthday"`
+	Genre     string    `json:"genre"`
+	Sport     string    `json:"sport"`
+	TeamName  string    `json:"team_name"`
 }
 
-type infoplayer []Player
+type Players []Player
+
+var Playerlist = Players{
+
+	{
+		ID:        "47f1923a-0227-11ed-b939-0242ac120002",
+		FirstName: "Ra",
+		LastName:  "TamaG",
+		Birthday:  time.Date(1998, time.June, 26, 12, 35, 0, 0, time.UTC),
+		Genre:     "M",
+		Sport:     "Cricket",
+		TeamName:  "Caballeros de Kolkata",
+	},
+	{
+		ID:        "04ef0cad-c366-486b-8a98-8480b7a57442",
+		FirstName: "Fernando",
+		LastName:  "Uribe",
+		Birthday:  time.Date(1988, time.January, 1, 11, 1, 0, 0, time.UTC),
+		Genre:     "M",
+		Sport:     "Futbol",
+		TeamName:  "Club Deportivo Popular Junior Fútbol Club S.A.",
+	},
+	{
+		ID:        "47f1946a-0227-11ed-b939-0242ac120002",
+		FirstName: "Imer",
+		LastName:  "Machado",
+		Birthday:  time.Date(1973, time.March, 26, 17, 16, 0, 0, time.UTC),
+		Genre:     "M",
+		Sport:     "Futbol",
+		TeamName:  "Naciopan",
+	},
+	{
+		ID:        "6ac7fa78-9b91-48c6-b8cf-45e917844d4b",
+		FirstName: "Legone",
+		LastName:  "James",
+		Birthday:  time.Date(1984, time.December, 30, 12, 9, 0, 0, time.UTC),
+		Genre:     "M",
+		Sport:     "Basketball",
+		TeamName:  "Lakers",
+	},
+	{
+		ID:        "f37c12fd-e710-4d9b-99fa-fd4c60931b57",
+		FirstName: "Mariana",
+		LastName:  "Pajon",
+		Birthday:  time.Date(1991, time.October, 10, 9, 12, 0, 0, time.UTC),
+		Genre:     "F",
+		Sport:     "BMX",
+		TeamName:  "Colombia",
+	},
+	{
+		ID:        "47f196cc-0227-11ed-b939-0242ac120002",
+		FirstName: "Russell",
+		LastName:  "Westbrick",
+		Birthday:  time.Date(1988, time.November, 12, 18, 7, 0, 0, time.UTC),
+		Genre:     "M",
+		Sport:     "Basketball",
+		TeamName:  "Lakers",
+	},
+	{
+		ID:        "0ae0bdf1-2a75-405d-9470-9f525c9b597e",
+		FirstName: "Catherine",
+		LastName:  "Ibarguen",
+		Birthday:  time.Date(1984, time.February, 12, 19, 12, 0, 0, time.UTC),
+		Genre:     "F",
+		Sport:     "Salto Triple",
+		TeamName:  "Colombia",
+	},
+	{
+		ID:        "47f196cc-0227-11ed-b939-0242ac120002",
+		FirstName: "Carlitos",
+		LastName:  "Cow",
+		Birthday:  time.Date(1998, time.June, 26, 9, 15, 0, 0, time.UTC),
+		Genre:     "M",
+		Sport:     "Futbol",
+		TeamName:  "Club Deportivo Popular Junior Fútbol Club S.A.",
+	},
+}
 
 func main() {
 
@@ -30,17 +106,20 @@ func main() {
 	})
 
 	r.HandleFunc("/players", func(w http.ResponseWriter, r *http.Request) {
-		playerinfo := infoplayer{
-			Player{"47f1923a-0227-11ed-b939-0242ac120002","Ra", "TamaG", 30, "M", "cricket", "Caballeros de Kolkata"},
-			Player{"04ef0cad-c366-486b-8a98-8480b7a57442","Fernando", "Uribe", 34, "M", "Futbol", "Club Deportivo Popular Junior Fútbol Club S.A."},
-			Player{"47f1946a-0227-11ed-b939-0242ac120002","Imer", "Machado", 49, "M", "Futbol", "Naciopan"},
-			Player{"6ac7fa78-9b91-48c6-b8cf-45e917844d4b","Legone", "James", 37, "M", "Basketball", "Lakers"},
-			Player{"f37c12fd-e710-4d9b-99fa-fd4c60931b57","Mariana", "Pajon", 30, "F", "Bmx", "Colombia"},
-			Player{"47f196cc-0227-11ed-b939-0242ac120002","Russell", "Wesbrick", 33, "M", "Basketball", "Lakers"},
-			Player{"0ae0bdf1-2a75-405d-9470-9f525c9b597e","Caterine", "Ibargüen", 38, "F", "Triple Salto", "Colombia"},
-			Player{"47f196cc-0227-11ed-b939-0242ac120002","Carlos", "Bacca", 35, "M", "Futbol", "Club Deportivo Popular Junior Fútbol Club S.A."},
+
+		json.NewEncoder(w).Encode(Playerlist)
+	})
+	r.HandleFunc("/players/{id}", func(w http.ResponseWriter, r *http.Request) {
+
+		vars := mux.Vars(r)
+		w.WriteHeader(http.StatusOK)
+
+		for _, player := range Playerlist {
+			if player.ID == vars["id"] {
+				json.NewEncoder(w).Encode(player)
+			}
 		}
-		json.NewEncoder(w).Encode(playerinfo)
+
 	})
 
 	err := http.ListenAndServe(":3000", r)
--- Ex/ex5/cmd/ex5/main.go
+++ Ex/ex5/cmd/ex5/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"time"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -8,18 +9,26 @@ import (
 )
 
 type Player struct {
-	Id string `json:"uuid"`
+	ID string `json:"uuid"`
 	FirstName string `json:"name"`
 	LastName  string `json:"last_name"`
-	Birthday  int    `json:"birthday"`
+	Birthday  time.Time    `json:"birthday"`
 	Genre     string `json:"genre"`
 	Sport     string `json:"sport"`
 	TeamName  string `json:"team_name"`
 }
 
-type infoplayer []Player
+type Players []Player
 
 func main() {
+	birthday1 := time.Date(1998, time.June, 26, 0, 0, 0, 0, time.UTC)
+	birthday2 := time.Date(1988, time.January, 1, 0, 0, 0, 0, time.UTC)
+	birthday3 := time.Date(1973, time.March, 26, 0, 0, 0, 0, time.UTC)
+	birthday4 := time.Date(1984, time.December, 30, 0, 0, 0, 0, time.UTC)
+	birthday5 := time.Date(1991, time.October, 10, 0, 0, 0, 0, time.UTC)
+	birthday6 := time.Date(1988, time.November, 12, 0, 0, 0, 0, time.UTC)
+	birthday7 := time.Date(1984, time.February, 12, 0, 0, 0, 0, time.UTC)
+	birthday8 := time.Date(1986, time.February, 8, 0, 0, 0, 0, time.UTC)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Go to /players")
@@ -27,15 +36,15 @@ func main() {
 
 	http.HandleFunc("/players", func(w http.ResponseWriter, r *http.Request) {
 
-		playerinfo := infoplayer{
-			Player{"47f1923a-0227-11ed-b939-0242ac120002","Ra", "TamaG", 30, "M", "cricket", "Caballeros de Kolkata"},
-			Player{"04ef0cad-c366-486b-8a98-8480b7a57442","Fernando", "Uribe", 34, "M", "Futbol", "Club Deportivo Popular Junior Fútbol Club S.A."},
-			Player{"47f1946a-0227-11ed-b939-0242ac120002","Imer", "Machado", 49, "M", "Futbol", "Naciopan"},
-			Player{"6ac7fa78-9b91-48c6-b8cf-45e917844d4b","Legone", "James", 37, "M", "Basketball", "Lakers"},
-			Player{"f37c12fd-e710-4d9b-99fa-fd4c60931b57","Mariana", "Pajon", 30, "F", "Bmx", "Colombia"},
-			Player{"47f196cc-0227-11ed-b939-0242ac120002","Russell", "Wesbrick", 33, "M", "Basketball", "Lakers"},
-			Player{"0ae0bdf1-2a75-405d-9470-9f525c9b597e","Caterine", "Ibargüen", 38, "F", "Triple Salto", "Colombia"},
-			Player{"47f196cc-0227-11ed-b939-0242ac120002","Carlos", "Bacca", 35, "M", "Futbol", "Club Deportivo Popular Junior Fútbol Club S.A."},
+		playerinfo := Players{
+			Player{"47f1923a-0227-11ed-b939-0242ac120002","Ra", "TamaG", birthday1, "M", "cricket", "Caballeros de Kolkata"},
+			Player{"04ef0cad-c366-486b-8a98-8480b7a57442","Fernando", "Uribe", birthday2, "M", "Futbol", "Club Deportivo Popular Junior Fútbol Club S.A."},
+			Player{"47f1946a-0227-11ed-b939-0242ac120002","Imer", "Machado", birthday3, "M", "Futbol", "Naciopan"},
+			Player{"6ac7fa78-9b91-48c6-b8cf-45e917844d4b","Legone", "James", birthday4, "M", "Basketball", "Lakers"},
+			Player{"f37c12fd-e710-4d9b-99fa-fd4c60931b57","Mariana", "Pajon", birthday5, "F", "Bmx", "Colombia"},
+			Player{"47f196cc-0227-11ed-b939-0242ac120002","Russell", "Wesbrick", birthday6, "M", "Basketball", "Lakers"},
+			Player{"0ae0bdf1-2a75-405d-9470-9f525c9b597e","Caterine", "Ibargüen", birthday7, "F", "Triple Salto", "Colombia"},
+			Player{"47f196cc-0227-11ed-b939-0242ac120002","Carlos", "Cow", birthday8, "M", "Futbol", "Club Deportivo Popular Junior Fútbol Club S.A."},
 		}
 
 		json.NewEncoder(w).Encode(playerinfo)
